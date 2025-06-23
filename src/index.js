e10bd1439e33b9866e2071dffe2eabf4998a570b
scriptlver <sloowlaah1@gmail.com>
2025-06-23 14:28:21 -0300

    siiiio

--- src/index.js
+++ src/index.js
@@ -18,10 +18,55 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock(){
+  let random = Math.random()
+  let result
+
+  switch(true){
+    case random < 0.33:
+      result = "RETA"
+      break;
+    case random < 0.66:
+      result = "CURVA"
+      break;  
+    default:
+      result = "CONFRONTO"
+      break;  
+  }
+
+  return result;
+}
+
 async function playRaceEngine(character1,character2){
   for(let round = 1; round <= 5; round++){
     console.log(`🏁 Rodada ${round}`);
+
     //sortear bloco
+    let block = await getRandomBlock();
+    console.log(`Bloco sorteado: ${block}`);
+  }
+
+  //rolar dados
+  let diceResult1 = await rollDice();
+  let diceResult2 = await rollDice();
+
+  //teste de habilidade
+  let totalTestSkill1 = 0;
+  let totalTestSkill2 = 0;
+
+  if((block === "RETA")){
+    totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+    totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+  }
+
+  if((block === "CURVA")){
+    totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+    totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+  }
+
+  if((block === "CONFRONTO")){
+    totalTestSkill1 = diceResult1 + character1.PODER;
+    totalTestSkill2 = diceResult2 + character2.PODER;
   }
 
 }
