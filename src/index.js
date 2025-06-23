12c01906b686f42151b38e7fa85703ba6ddbccb6
scriptlver <sloowlaah1@gmail.com>
2025-06-23 14:17:15 -0300

    simm

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 const player1 = {
-    NOME : "Mario",
-    VELOCIDADE: 4,
-    MANOBRABILIDADE: 3,
-    PODER: 3,
-    PONTOS: 0,
-}
+  NOME: "Mario",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0,
+};
 
 const player2 = {
-    NOME : "Luigi",
-    VELOCIDADE: 3,
-    MANOBRABILIDADE: 4,
-    PODER: 4,
-    PONTOS: 0,
+  NOME: "Luigi",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0,
+};
+
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-async function rollDice(){
-    return Math.floor(Math.random() * 6) + 1; 
+async function playRaceEngine(character1,character2){
+  for(let round = 1; round <= 5; round++){
+    console.log(`🏁 Rodada ${round}`);
+    //sortear bloco
+  }
+
 }
 
-(async function main(){
-    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
+(async function main() {
+  console.log(
+    `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`
+  );
+
+  await playRaceEngine(player1, player2);
 })();
